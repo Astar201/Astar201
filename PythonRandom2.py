b197899a6cca93dec217c693ae63890820015b45
Astar201 <2025astar201@gmail.com>
2024-10-22 16:42:15 +0300

    Add files via upload
    First game version - Beta 0.9

---
+++ PythonRandom2.py
@@ -0,0 +1,11 @@
+import random
+
+Level = input(("Введите сложность (от 1 до 3): "))
+
+if Level == 1:
+    print("Отлично! Вы выбрали легкую сложность.")
+    Attempt1_1 = input("Введите ваше предположение (от 1 до 100): ")
+
+Value = random.randint(1, 100)
+
+Attept1 = input()
